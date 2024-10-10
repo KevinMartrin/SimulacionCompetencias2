1a8152dfedafa008b868fd0163f14ed859e7b4fb
KevinMartrin <kevinjtrinidad@gmail.com>
2024-10-10 09:00:55 -0600

    Clase asignacion

--- SimulacionCompetencias/Classes/Asignacion.cs
+++ SimulacionCompetencias/Classes/Asignacion.cs
@@ -6,7 +6,21 @@ using System.Threading.Tasks;
 
 namespace SimulacionCompetencias.Classes
 {
-    internal class Asignacion
+    public class Asignacion
     {
+        public double latitud { get; set; }
+        public double longitud { get; set; }
+        public int idPunto { get; set; }
+        public bool activo { get; set; } //Para ver si hay o no un arbol
+        public string especie { get; set; }
+        public Asignacion() { }
+        public Asignacion(Asignacion a_asignacion)
+        {
+            latitud = a_asignacion.latitud;
+            longitud = a_asignacion.longitud;
+            idPunto = a_asignacion.idPunto;
+            activo = a_asignacion.activo;
+            especie = a_asignacion.especie;
+        }
     }
 }
